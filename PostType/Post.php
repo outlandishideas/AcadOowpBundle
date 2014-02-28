3cd62252a9c99f351bac205f92a5283e885baf66
Hanna <hanna@outlandish.com>
2014-02-28 15:38:47 +0000

    changes to the initial setup

--- PostType/Post.php
+++ PostType/Post.php
@@ -7,4 +7,15 @@ use Outlandish\OowpBundle\PostType\RoutemasterPost as BasePost;
 
 abstract class Post extends BasePost {
 
+    /**
+     * This is a new method
+     * Created By: Hanna
+     * Created On: 28/02/2014
+     * Modified By: Hanna
+     * Modified On: 29/02/2014
+     * @return string
+     */
+    public function newMethod(){
+        return "This is new";
+    }
 } 
