21c4e49d3a167256f8be98bf1c707f9ae5353b2f
Georgi Griffiths <georgi@outlandish.com>
2014-06-11 11:31:02 +0100

    made inheritance of navigation stuff more consistent

--- Controller/NavigationController.php
+++ Controller/NavigationController.php
@@ -9,15 +9,66 @@ use Outlandish\AcadOowpBundle\Controller\DefaultController as BaseController;
 class NavigationController extends BaseController {
 
     public function renderFooterAction(){
+        $args = $this->generateFooterArguments();
+        return $this->render(
+            'OutlandishAcadOowpBundle:Navigation:footer.html.twig',
+            $args
+        );
+    }
+
+	public function renderMenuAction( $maxDepth = 1 ){
+
+        $args = $this->generateMenuArguments($maxDepth);
+
+		return $this->render(
+			'OutlandishAcadOowpBundle:Menu:menuItems.html.twig',
+            $args
+		);
+	}
+
+    public function generateMenuArguments($maxDepth = 1)
+    {
+        $this->queryManager = $this->get('outlandish_oowp.query_manager');
+        $this->postManager = $this->get('outlandish_oowp.post_manager');
+        $rootPost = null;
+        $postType = 'page';
+
+        $queryArgs = array(
+            'post_type' => $postType,
+            'orderby' => 'menu_order',
+            'order' => 'asc'
+        );
+        if ($rootPost) {
+            $posts = $rootPost->children($queryArgs);
+        } else {
+            $class = $this->postManager->postTypeClass($postType);
+            $queryArgs['post_parent'] = $class::postTypeParentId();
+            $posts = $this->queryManager->query($queryArgs);
+        }
+
+        $menuArgs = array(
+            'max_depth' => $maxDepth,
+            'current_depth' => 1
+        );
+
+        return array(
+            'posts' => $posts,
+            'queryArgs' => $queryArgs,
+            'menuArgs' => (object)$menuArgs
+        );
+    }
+
+    public function generateFooterArguments()
+    {
         $this->queryManager = $this->get('outlandish_oowp.query_manager');
         $this->postManager = $this->get('outlandish_oowp.post_manager');
 
         $homePage = $this->queryManager->query(array(
             'page_id' => get_option('page_on_front')
-            ))->post;
+        ))->post;
 
         $pages = $this->queryManager->query(array(
-           'post_type' => 'page',
+            'post_type' => 'page',
             'orderby' => 'menu_order',
             'order' => 'asc'
         ));
@@ -27,56 +78,15 @@ class NavigationController extends BaseController {
         $socialMedia = get_field('social_media', 'options');
         $address = get_field('address', 'options');
         $phoneNumber = get_field('phone_number', 'options');
-        return $this->render(
-            'OutlandishAcadOowpBundle:Navigation:footer.html.twig',
-            array(
-                'sections' => $sections,
-                'socialmedia' => $socialMedia,
-                'pages' => $pages->posts,
-                'organisations' => $organisations,
-                'address' => $address,
-                'phonenumber' => $phoneNumber
-            )
+
+        return array(
+            'sections' => $sections,
+            'socialmedia' => $socialMedia,
+            'pages' => $pages->posts,
+            'organisations' => $organisations,
+            'address' => $address,
+            'phonenumber' => $phoneNumber
         );
     }
-
-	public function renderMenuAction( $maxDepth = 1 ){
-
-		//$menu = $this->get('outlandish_oowp.helper.menu');
-		//return $this->render( $menu->render() );
-
-		$this->queryManager = $this->get('outlandish_oowp.query_manager');
-		$this->postManager = $this->get('outlandish_oowp.post_manager');
-		$rootPost = null;
-		$postType = 'page';
-
-		$queryArgs = array(
-			'post_type' => $postType,
-			'orderby' => 'menu_order',
-			'order' => 'asc'
-		);
-		if ($rootPost) {
-			$posts = $rootPost->children($queryArgs);
-		} else {
-			$class = $this->postManager->postTypeClass($postType);
-			$queryArgs['post_parent'] = $class::postTypeParentId();
-			$posts = $this->queryManager->query($queryArgs);
-		}
-
-		$menuArgs = array(
-			'max_depth' => $maxDepth,
-			'current_depth' => 1
-		);
-
-		return $this->render(
-			'OutlandishAcadOowpBundle:Menu:menuItems.html.twig',
-			array(
-				'posts' => $posts,
-				'queryArgs' => $queryArgs,
-				'menuArgs' => (object)$menuArgs
-			)
-		);
-
-	}
 	
 }
