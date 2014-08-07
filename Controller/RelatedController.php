2aebc50dcee53be1ea6a1c38df113ac8760a8a40
Iain <iain@polevaultweb.com>
2014-08-07 16:40:07 +0100

    Related content on people post

--- Controller/RelatedController.php
+++ Controller/RelatedController.php
@@ -3,19 +3,31 @@
 
 namespace Outlandish\AcadOowpBundle\Controller;
 
+use Outlandish\AcadOowpBundle\Controller\DefaultController as BaseController;
+use Outlandish\AcadOowpBundle\PostType\Theme;
+use Outlandish\AcadOowpBundle\PostType\Post;
+use Outlandish\AcadOowpBundle\PostType\Person;
 
-class RelatedController {
 
+class RelatedController extends BaseController{
+
+	private function unsetByValue( &$array, $value) {
+		if ( ( $key = array_search( $value, $array ) ) !== false ) {
+			unset( $array[$key] );
+		}
+	}
 	public function renderResourcesAction( $post ) {
 		$types = Theme::childTypes( false );
-		unset( $types['people'] );
+		$this->unsetByValue( $types, 'person' );
+		$this->unsetByValue( $types, 'role' );
+
 		$connected_types = $post->connectedTypes( $types );
 
-
+		$items = $post->connected( $connected_types );
 
 		return $this->render(
-					'OutlandishAcadOowpBundle:Related:items.html.twig',
-						$items
+			'OutlandishAcadOowpBundle:Default:items.html.twig',
+			array( 'items' => $items )
 		);
 
 	}
--- PostType/Post.php
+++ PostType/Post.php
@@ -108,11 +108,13 @@ abstract class Post extends BasePost {
 
 	public static function childTypes( $isChild = true ) {
 		$types = array();
+		$self = get_called_class();
 		foreach (self::$postManager->postTypeMapping() as $postType => $class) {
-			if ( $class instanceof self && $isChild ) {
+
+			if ( in_array( $self, class_parents( $class) ) && $isChild ) {
 				// if the class is a child of self and we are looking for children
 				$types[] = $postType;
-			} else if (	! $class instanceof self && ! $isChild) {
+			} else if (	! in_array( $self, class_parents( $class) ) && ! $isChild) {
 				// if class not child of self and we are not looking for children
 				$types[] = $postType;
 			}
