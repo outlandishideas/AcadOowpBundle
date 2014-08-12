5f0679c52dcc2aed4ffd8ede4f5e50fad1f9c085
Iain <iain@polevaultweb.com>
2014-08-12 17:55:07 +0100

    AOO-157 Integrate menus with WordPress menus

--- OutlandishAcadOowpBundle.php
+++ OutlandishAcadOowpBundle.php
@@ -8,4 +8,30 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OutlandishAcadOowpBundle extends Bundle {
 
+
+	function __construct() {
+
+		add_theme_support( 'menus' );
+
+		add_action(
+			'init', function () {
+				register_nav_menus(
+					array(
+						'header' => __( 'Header Navigation' ),
+						'footer' => __( 'Footer Navigation' )
+					)
+				);
+			}
+		);
+		add_action(
+			'admin_menu', function () {
+				remove_menu_page( 'themes.php' );
+			}
+		);
+		add_action(
+			'admin_menu', function () {
+				add_menu_page('Menus', 'Menus', 'manage_options', 'nav-menus.php', '', 'dashicons-admin-appearance', 64);
+			}
+		);
+	}
 }
