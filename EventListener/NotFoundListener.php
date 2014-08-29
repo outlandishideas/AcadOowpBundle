22ececd1dc022b6f2331d155b5092fab400a5a3e
Hanna <hannalaakso123@yahoo.co.uk>
2014-08-29 12:39:55 +0100

    AOO-201 404 page needs creating //still not rendering properly

---
+++ EventListener/NotFoundListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Outlandish\AcadOowpBundle\EventListener;
+
+use Outlandish\OowpBundle\PostType\FakePost;
+use Symfony\Bundle\FrameworkBundle\Templating\PhpEngine;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class NotFoundListener {
+
+	/** @var PhpEngine */
+	private $templateEngine;
+
+	function __construct(PhpEngine $templateEngine = null) {
+		$this->templateEngine = $templateEngine;
+	}
+
+	public function onKernelException(GetResponseForExceptionEvent $event) {
+		if ($event->getException() instanceof NotFoundHttpException) {
+
+			// replace the global post with a fake one, and show a 404 template
+			global $post, $wp_query;
+			$post = new FakePost();
+            $wp_query->post = $post;
+
+            $content = $this->templateEngine->render('OutlandishAcadOowpBundle:Default:404.html.twig', array('post'=> $post));
+
+            $event->setResponse(new Response($content));
+		}
+	}
+}
