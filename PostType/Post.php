e99f7f438a07cd35383e462061b8203101e64ca7
Hanna <hannalaakso123@yahoo.co.uk>
2014-09-01 06:05:13 +0100

    Show date only events and news. Return an array for connected post types.

--- PostType/Post.php
+++ PostType/Post.php
@@ -99,7 +99,7 @@ abstract class Post extends BasePost {
             } else {
                 $date = $this->startDateString($format);
             }
-        } else {
+        } elseif ($this->post_type == News::postType()) {
             $date = date($format, strtotime($this->post_date));
         }
 
@@ -135,97 +135,109 @@ abstract class Post extends BasePost {
 
 
     /**
-     * Returns custom posts, grouped and titled by post type, connected to current post
+     * Returns posts, grouped and titled by post type, connected to current post
      * @return array
      */
     public function allConnectedPosts() {
         $allConnectedPosts = array(
-            array(
-                'title' => 'Related themes',
-                'items' => $this->connectedThemes()
-            ),
-            array(
-                'title' => 'Related documents',
-                'items' => $this->connectedDocuments()
-            ),
-            array(
-                'title' => 'Related events',
-                'items' => $this->connectedEvents()
-            ),
-            array(
-                'title' => 'Related news',
-                'items' => $this->connectedNews()
-            ),
-            array(
-                'title' => 'Related people',
-                'items' => $this->connectedPeople()
-            ),
-            array(
-                'title' => 'Related places',
-                'items' => $this->connectedPlaces()
-            ),
-            array(
-                'title' => 'Related projects',
-                'items' => $this->connectedProjects()
-            )
+            $this->connectedThemes(),
+            $this->connectedDocuments(),
+            $this->connectedEvents(),
+            $this->connectedPeople(),
+            $this->connectedPlaces(),
+            $this->connectedProjects()
         );
         return $allConnectedPosts;
     }
 
+
     /**
-     * Returns all Document posts connected to current post
-     * @return null|\Outlandish\OowpBundle\PostType\Post|\Outlandish\OowpBundle\Query\OowpQuery
+     * Returns a title and all document posts connected to current post
+     * @param string $title
+     * @return array
      */
-    public function connectedDocuments() {
-        return $this->connected(Document::postType());
+    public function connectedDocuments($title = 'Related documents') {
+        return array(
+            'title' => $title,
+            'items' => $this->connected(Document::postType())
+        );
+    }
+
+
+    /**
+     * Returns a title and all event posts connected to current post
+     * @param string $title
+     * @return array
+     */
+    public function connectedEvents($title = 'Related events') {
+        return array(
+            'title' => $title,
+            'items' => $this->connected(Event::postType())
+        );
     }
 
     /**
-     * Returns all Event posts connected to current post
-     * @return null|\Outlandish\OowpBundle\PostType\Post|\Outlandish\OowpBundle\Query\OowpQuery
+     * Returns a title and all news posts connected to current post
+     * @param string $title
+     * @return array
      */
-    public function connectedEvents() {
-        return $this->connected(Event::postType());
+    public function connectedNews($title = 'Related news') {
+        return array(
+            'title' => $title,
+            'items' => $this->connected(News::postType())
+        );
     }
 
-    /**
-     * Returns all News posts connected to current post
-     * @return null|\Outlandish\OowpBundle\PostType\Post|\Outlandish\OowpBundle\Query\OowpQuery
-     */
-    public function connectedNews() {
-        return $this->connected(News::postType());
-    }
 
     /**
-     * Returns all Person posts connected to current post
-     * @return null|\Outlandish\OowpBundle\PostType\Post|\Outlandish\OowpBundle\Query\OowpQuery
+     * Returns a title and all person posts connected to current post
+     * @param string $title
+     * @return array
      */
-    public function connectedPeople() {
-        return $this->connected(Person::postType());
+    public function connectedPeople($title = 'Related people') {
+        return array(
+            'title' => $title,
+            'items' => $this->connected(Person::postType())
+        );
     }
 
-    /**
-     * Returns all Place posts connected to current post
-     * @return null|\Outlandish\OowpBundle\PostType\Post|\Outlandish\OowpBundle\Query\OowpQuery
-     */
-    public function connectedPlaces() {
-        return $this->connected(Place::postType());
-    }
 
     /**
-     * Returns all Project posts connected to current post
-     * @return null|\Outlandish\OowpBundle\PostType\Post|\Outlandish\OowpBundle\Query\OowpQuery
+     * Returns a title and all place posts connected to current post
+     * @param string $title
+     * @return array
      */
-    public function connectedProjects()  {
-        return $this->connected(Project::postType());
+    public function connectedPlaces($title = 'Related places') {
+        return array(
+            'title' => $title,
+            'items' => $this->connected(Place::postType())
+        );
     }
 
+
     /**
-     * Returns all Theme posts connected to current post
-     * @return null|\Outlandish\OowpBundle\PostType\Post|\Outlandish\OowpBundle\Query\OowpQuery
+     * Returns a title and all project posts connected to current post
+     * @param string $title
+     * @return array
      */
-    public function connectedThemes() {
-        return $this->connected(Theme::postType());
+    public function connectedProjects($title = 'Related projects') {
+        return array(
+            'title' => $title,
+            'items' => $this->connected(Project::postType())
+        );
+    }
+
+
+    /**
+     * Returns a title and all theme posts connected to current post
+     * @param string $title
+     * @return array
+     */
+    public function connectedThemes($title = 'Related themes') {
+        return array(
+            'title' => $title,
+            'items' => $this->connected(Theme::postType())
+        );
     }
 
 }
