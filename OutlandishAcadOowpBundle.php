f9aac2900879adcab87018d478a4f7cb6351cfc9
Matthew Kendon <mkendon@gmail.com>
2014-09-23 23:20:09 +0100

    don't do wordpress things untill wordpress is installed

--- OutlandishAcadOowpBundle.php
+++ OutlandishAcadOowpBundle.php
@@ -11,33 +11,35 @@ class OutlandishAcadOowpBundle extends Bundle {
 
 	function __construct() {
 
-		add_theme_support( 'menus' );
+        if(function_exists('add_theme_support') ) {
+            add_theme_support('menus');
+            register_nav_menus(
+                array(
+                    'header' => __('Header Navigation'),
+                    'footer' => __('Footer Navigation'),
+                    'footer_about' => __('Footer About')
+                )
+            );
+        }
 
-        add_image_size( 'avatar-square', '100', '100', array('center', 'center'));
+        if(function_exists('add_image_size')) {
+            add_image_size('avatar-square', '100', '100', array('center', 'center'));
+            add_image_size('item-image-12', '630', '380', array('center', 'center'));
+            add_image_size('item-image-6', '307', '190', array('center', 'center'));
+            add_image_size('item-image-4', '198', '122', array('center', 'center'));
+        }
 
-        add_image_size( 'item-image-12', '630', '380', array('center', 'center'));
-
-        add_image_size( 'item-image-6', '307', '190', array('center', 'center'));
-
-        add_image_size( 'item-image-4', '198', '122', array('center', 'center'));
-
-        register_nav_menus(
-			array(
-				'header' => __( 'Header Navigation' ),
-				'footer' => __( 'Footer Navigation' ),
-                'footer_about'=> __( 'Footer About' )
-			)
-		);
-
-		add_action(
-			'admin_menu', function () {
-				remove_menu_page( 'themes.php' );
-			}
-		);
-		add_action(
-			'admin_menu', function () {
-				add_menu_page('Menus', 'Menus', 'manage_options', 'nav-menus.php', '', 'dashicons-admin-appearance', 64);
-			}
-		);
+        if(function_exists('add_action')){
+            add_action(
+                'admin_menu', function () {
+                    remove_menu_page( 'themes.php' );
+                }
+            );
+            add_action(
+                'admin_menu', function () {
+                    add_menu_page('Menus', 'Menus', 'manage_options', 'nav-menus.php', '', 'dashicons-admin-appearance', 64);
+                }
+            );
+        }
 	}
 }
