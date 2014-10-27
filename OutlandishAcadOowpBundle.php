effc9950cbcf99a332f365708aadd49579276d58
Hanna <hannalaakso123@yahoo.co.uk>
2014-10-27 12:21:12 +0000

    AOO-266 php error on Android // printing out video attributes

--- OutlandishAcadOowpBundle.php
+++ OutlandishAcadOowpBundle.php
@@ -53,11 +53,13 @@ class OutlandishAcadOowpBundle extends Bundle {
         global $post;
         $video = '';
         print 'test!';
-        if ($videoURL = $post->metadata('youtube_video_url')) {
-            $id = str_replace('http://vimeo.com/', '', $videoURL);
-            $video = '<iframe class="video-iframe" src="//player.vimeo.com/video/'. $id .'?title=0&amp;byline=0&amp;portrait=0&amp;color=ff9933" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-        }
-        return $video;
+        print_r($atts);
+//        if ($videoURL = $post->metadata('youtube_video_url')) {
+//            $id = str_replace('http://vimeo.com/', '', $videoURL);
+//            $video = '<iframe class="video-iframe" src="//player.vimeo.com/video/'. $id .'?title=0&amp;byline=0&amp;portrait=0&amp;color=ff9933" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+//        }
+        //return $video;
+        return;
     }
 
 }
