097ffc5ecb680d552309851b4af60d1fdab8e6e5
Hanna <hannalaakso123@yahoo.co.uk>
2015-01-08 14:00:29 +0000

    Change acf for qmul header text.

--- Controller/NavigationController.php
+++ Controller/NavigationController.php
@@ -259,7 +259,7 @@ class NavigationController extends SearchController {
                 'header_image' => get_field('header_image', 'options'),
                 'header_text' => get_field('header_text', 'options'),
                 'qmul_image' => get_field('qmul_image', 'options'),
-                'qmul_text' => get_field('qmul_text', 'options'),
+                'qmul_text' => get_field('qmul_text_header', 'options'),
                 'qmul_link' => get_field('qmul_link', 'options'),
             )
         );
