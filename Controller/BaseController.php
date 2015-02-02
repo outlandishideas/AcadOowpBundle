f3ab0b0e23dda5078b74f5127ee87ad67022b6fa
Matthew <mkendon@gmail.com>
2015-02-02 17:23:12 +0000

    added some helper functions to BaseController

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -9,9 +9,14 @@
 namespace Outlandish\AcadOowpBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Outlandish\OowpBundle\PostType\Post;
 
 class BaseController extends \Outlandish\RoutemasterBundle\Controller\BaseController {
 
+    /**
+     * @param string $searchQuery search query that will be converted into a post_name
+     * @return null|Post
+     */
     protected function featuredPost($searchQuery)
     {
         $name = sanitize_title($searchQuery, null);
@@ -19,12 +24,29 @@ class BaseController extends \Outlandish\RoutemasterBundle\Controller\BaseContro
         return ($results->post_count == 1) ? $results->post : null;
     }
 
-    protected function queryPage($id)
+    /**
+     * @param $name - slug of the post to get
+     * @param string|array $postTypes array or string or post types to search in - defaults to any
+     * @param bool $redirectCanonical whether to redirect to the canonical link
+     * @return Post|null
+     */
+    protected function queryPost($name, $postTypes = 'any', $redirectCanonical = false)
+    {
+        $slugBits = explode('/', trim($name, '/'));
+        return $this->querySingle(array('name' => $slugBits[count($slugBits) - 1], 'post_type' => $postTypes), $redirectCanonical);
+    }
+
+    /**
+     * @param int $id ID of the page
+     * @param bool $redirectCanonical whether to redirect to the canonical link
+     * @return Post|null
+     */
+    protected function queryPage($id, $redirectCanonical = false)
     {
         return $this->querySingle(array(
             'page_id' => $id,
             'post_type' => 'page'
-        ));
+        ), $redirectCanonical);
     }
 
 
--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -7,24 +7,20 @@ namespace Outlandish\AcadOowpBundle\Controller;
 use Outlandish\OowpBundle\PostType\Post as BasePost;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends \Outlandish\RoutemasterBundle\Controller\BaseController {
+class DefaultController extends BaseController {
 
 	/**
 	 * Route is specified in routing.yml because it needs to come last
      *
      * @param mixed $slugs
      * @return array
-	 * @Template("OutlandishAcadOowpBundle:Default:post.html.twig")
 	 */
 	public function defaultPostAction($slugs)
     {
-		$slugBits = explode('/', trim($slugs, '/'));
-        /** @var BasePost $post */
-		$post = $this->querySingle(array('name' => $slugBits[count($slugBits) - 1], 'post_type' => 'any'), true);
-
-		return array(
-            'post' => $post,
-        );
+		$post = $this->queryPost($slugs, 'any', true);
+		return $this->render('OutlandishAcadOowpBundle:Default:post.html.twig', array(
+            'post' => $post
+        ));
 	}
 
 }
